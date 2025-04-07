8fdb62a533dc6695b5893c4b7b22f9d4e77df89b
soplima <sop.lima13@gmail.com>
2025-04-07 11:58:59 -0300

    ex03

--- BCC4001/POO/list_CPP/SymmetricTree.h
+++ BCC4001/POO/list_CPP/SymmetricTree.h
@@ -1,3 +1,6 @@
+#include <vector>
+using namespace std;
+
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -7,8 +10,21 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 class Solution {
+    private:
+        bool isMirror(TreeNode* t1, TreeNode* t2){
+            if(!t1 && !t2) return true;
+            if(!t1 || !t2) return false;
+            
+            return (t1->val == t2->val) &&
+                isMirror(t1->left,t2->right) &&
+                isMirror(t1->right, t2->left);
+        }
+
     public:
         bool isSymmetric(TreeNode* root) {
-            
-        }
+            if (!root) return true;
+            return isMirror(root->left, root->right);
+        }  
+        
+    
     };
