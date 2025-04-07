436751e4d234d63b50f5e15cc970bd91632b3d1d
soplima <sop.lima13@gmail.com>
2025-04-07 10:38:30 -0300

    ex02

---
+++ BCC4001/POO/list_CPP/BinaryTreeInorderTraversal.h
@@ -0,0 +1,28 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+ #include <vector>
+ using namespace std;
+
+class Solution {
+    private:
+    void inorder(TreeNode* node, vector<int>& result) {
+        if(!node) return;
+        inorder(node->left, result);
+            result.push_back(node->val);
+        inorder(node->right, result);
+    }
+
+    public:
+        vector<int> inorderTraversal(TreeNode* root) {
+            vector<int> result;
+            inorder(root, result);
+            return result;
+        }
+    };
