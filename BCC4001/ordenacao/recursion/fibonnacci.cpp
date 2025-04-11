4eda0f0c57e96d0f15bcaadbdf623634ebc2d0ce
soplima <sop.lima13@gmail.com>
2025-04-10 23:06:50 -0300

    fibonacci

--- BCC4001/ordenacao/recursion/fibonnacci.cpp
+++ BCC4001/ordenacao/recursion/fibonnacci.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int fibonacci(int n){
+    if(n == 0){
+        return 0;
+    }
+    if(n == 1){
+        return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main(){
+    int n;
+    cout << "Digite um número inteiro: ";
+    cin >> n;
+
+    cout << "Fibonacci de " << n << " é: " << fibonacci(n) << endl;
+
+    return 0;
+}
