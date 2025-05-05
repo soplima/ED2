25c3f9a89efb78b044396bb920cad74b84e54344
soplima <sop.lima13@gmail.com>
2025-05-05 20:40:16 -0300

    feat: sorting algorithms implemented

---
+++ BCC4001/problemas/ordenacao/1.2.cpp
@@ -0,0 +1,56 @@
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include "utils.h"
+#include <vector>
+
+
+#include <algorithm>
+
+#define DEBUG 1
+
+// retorna o k-ésimo maior elemento do vetor.
+// Os elementos de v podem ser permutados durante a execução.
+// pré-condição: o vetor não possui elementos repetidos
+// k=1 retorna o maior elemento
+// k=2 retorna o segundo maior elemento
+// k=....
+// k=n retorna o menor elemento
+
+int maior(std::vector<int> &v, int first, int last){
+    int max = first;
+    for(int i = first+1; i <=last; i++){
+        if(v[i] > v[max]){
+            max = i;
+        }
+    }
+    return max;
+}
+
+int k_maior(std::vector<int> &v, int k){
+    int n = v.size();
+    for(int i = 0; i < k; i++){
+        int imax = maior(v, i, n-1);
+        std::swap(v[i],v[imax]);
+    }
+    return v[k-1];
+}
+
+int main(int argc, char** argv){
+    int n = atoi(argv[1]);
+    int k = atoi(argv[2]);
+
+    auto v = gerar_vetor_aleatorio_sem_reps(n, 42);
+
+    #if DEBUG
+    imprimir_vetor(v.data(), n);
+    #endif
+
+    int res = k_maior(v, k);
+
+    #if DEBUG
+    std::cout << "O " << k << "-ésimo maior elemento do vetor é o " << res << "!\n";
+    #endif    
+
+    return EXIT_SUCCESS;
+}
