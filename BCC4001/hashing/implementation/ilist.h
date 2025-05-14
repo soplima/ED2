4019cf39295a861290abc29cfccadf693d13b82e
soplima <sop.lima13@gmail.com>
2025-05-14 13:20:05 -0300

    feat: struct definition

---
+++ BCC4001/hashing/implementation/ilist.h
@@ -0,0 +1,27 @@
+#ifndef __ILIST_H__
+#define __ILIST_H__
+
+typedef struct INOH
+{
+    int chave;
+    int valor;
+    int ocupado;
+} INOH;
+
+typedef struct ILIST
+{
+    INOH *nos;
+    int max;
+    int tam;
+    int alloc;
+    int alloc_step;
+} ILIST;
+
+ILIST* ILIST_Criar(int alloc_step);
+int ILIST_inserir(ILIST *L, int chave, int valor);
+int ILIST_Remover(ILIST *L, int chave);
+int ILIST_Buscar(ILIST *L, int chave);
+void ILIST_Imprimir(ILIST *L, int debug);
+INOH* ILIST_Endereco(ILIST *L, int pos);
+
+#endif 
