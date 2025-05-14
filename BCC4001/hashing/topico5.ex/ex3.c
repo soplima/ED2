aa7eb8b4f8d0f7b8f27bff47f356f1026a0cdb8b
soplima <sop.lima13@gmail.com>
2025-05-14 18:47:50 -0300

    feat: hashing algorithm implementation

---
+++ BCC4001/hashing/topico5.ex/ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Seja n o comprimento da string. Encontre duas funções 
+// (matemáticas) fa(n) e fb(n) que calcule o número
+// de somas e multiplicações realizadas pelos métodos implementados 
+// nos exercícios a e b. Compare os fa(n) e
+// fb(n) para n = 5, 10, 20, 100.
+
+unsigned int string_hash(char* string, int M){
+    int hash = 0;
+    int k = 31;
+    
+}
