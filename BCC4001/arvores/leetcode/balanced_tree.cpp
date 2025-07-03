16045c80ef4d8cc2c020f8f1e2f4ff0e5d6a9a41
soplima <sop.lima13@gmail.com>
2025-07-03 12:10:46 -0300

    feat: returns bool value to check if a binary tree is balanced or not

--- BCC4001/arvores/leetcode/balanced_tree.cpp
+++ BCC4001/arvores/leetcode/balanced_tree.cpp
@@ -1,7 +1,5 @@
-typedef struct noh_ab{
-    struct noh_ab *esq, *dir;
-    int chave;
-}NOH_AB;
+#include <algorithm>
+#include "BST_tree.h"
 
 int alturaBalanceada(NOH_AB *no) {
     if (!no) return 0;
@@ -9,9 +7,9 @@ int alturaBalanceada(NOH_AB *no) {
     if (altEsq == -1) return -1;
     int altDir = alturaBalanceada(no->dir);
     if (altDir == -1) return -1;
-    if (abs(altEsq - altDir) > 1)
+    if (std::abs(altEsq - altDir) > 1)
         return -1;
-    return 1 + max(altEsq, altDir);
+    return 1 + std::max(altEsq, altDir);
 }
 
 int balanceada(NOH_AB *A) {
